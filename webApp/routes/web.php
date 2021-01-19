40bf8d015cfea304c0dd6f4159482a282045eab2
tanvir-tech <tanvirah325@gmail.com>
2021-01-19 10:37:23 +0600

    fillable added and route edited

--- webApp/routes/web.php
+++ webApp/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students',[StudentController::class,'index']);
+
+Route::post('/register',[StudentController::class,'store'])->name('students.store');
