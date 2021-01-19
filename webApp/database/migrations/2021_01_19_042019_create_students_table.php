79fa924d69e56d4f1a73fe2656b8ab05eb1542ba
tanvir-tech <tanvirah325@gmail.com>
2021-01-19 10:25:21 +0600

    New columns added in migration file

--- webApp/database/migrations/2021_01_19_042019_create_students_table.php
+++ webApp/database/migrations/2021_01_19_042019_create_students_table.php
@@ -15,6 +15,11 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('address');
+            $table->string('password');
             $table->timestamps();
         });
     }
